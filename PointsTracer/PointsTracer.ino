463692141f13de978392d359f8da45e1db397b85
Hugo García <hugo_os_95@hotmail.com>
2017-04-22 16:28:43 -0500

    Added support for diagonal traces and coordinates parsing - Hugo García

--- PointsTracer/PointsTracer.ino
+++ PointsTracer/PointsTracer.ino
@@ -1,11 +1,13 @@
 #include "EZStepper.h"
 #include <math.h>
+#include <Servo.h>
 
 //                                              //Constructor:
-//                                              //    1.  Steps per revolution
+//                                              //    1. Steps per revolution
 //                                              //    2. to 5. Driver pins.
 EZStepper stepperY(2094, 8, 9, 10 , 11);
 EZStepper stepperX(2094, 4, 5, 6, 7);
+Servo servo;
 int intYButtonPin = 2;
 int intXButtonPin = 3;
 int intCurrentX = 0;
@@ -18,31 +20,41 @@ struct point
 };
 
 typedef struct point Point;
+
+//===================================================================================================================
+//                                                      //GLOBAL VARIABLES
 int INT_X_SENSIVITY = 100;
 int INT_Y_SENSIVITY = 100;
-
-//variables de transformacion
+//                                                      //Square
 String datos = "9,10,9,11,9,12,9,13,10,13,11,13,12,13,12,12,12,11,12,10,11,10,10,10,9,10";
-int numOfPoints = 13;
+int intPointsNumber = 13;
+//                                                      //Triangle
+//String datos = "9,10,12,15,15,10,9,10";
+// intPointsNumber 4;
+
 int count = 0;
 String digito = "";
-//-----------------
 
+//===================================================================================================================
+//                                                      //VOID SETUP
 void setup() 
 {
   Serial.begin(9600);
-  //                                            //Set the velocity to 64 half-steps per second.
+  servo.attach(12);
+  servo.write(21);
+  delay(1000);
+  //                                                    //Set the velocity to 100 half-steps per second.
   pinMode(intXButtonPin, INPUT);
   pinMode(intYButtonPin, INPUT);
+
   stepperX.subSetStatesPerSec(100);
   stepperY.subSetStatesPerSec(100);
   
-  Point pointsArr[numOfPoints];
+  Point pointsArr[intPointsNumber];
   getCoord(pointsArr);
-  Serial.print ("volvi");
   stepperX.subSetDirection(stepperX.LEFT);
   stepperY.subSetDirection(stepperY.LEFT);
- /* while (!(digitalRead(intXButtonPin) == HIGH))
+  while (!(digitalRead(intXButtonPin) == HIGH))
   {
     stepperX.subMoveBySteps(1);
   }
@@ -50,22 +62,54 @@ void setup()
   while (!(digitalRead(intYButtonPin) == HIGH))
   {
     stepperY.subMoveBySteps(1);
-  }*/
+  }
 
   subTracePoints(pointsArr);
 }
 
-void loop() 
+//-----------------------------------------------------------------------------------------------------------------
+//                                                      //VOID SETUP METHODS
+
+//                                                      //Obtain the coordinates of the data String.
+void getCoord(Point points[])
 {
+  while (datos.length() > 0)
+  {
+    while (isDigit(datos.charAt(0)))
+    {
+      digito += datos.charAt(0);
+      datos.remove(0,1);
+    }
+    
+    points[count].intX = digito.toInt(); 
+    datos.remove(0,1);
+    digito = "";
   
+    while (isDigit(datos.charAt(0)))
+    {
+      digito += datos.charAt(0);
+      datos.remove(0,1);
+    }
+    
+    points[count].intY = digito.toInt(); 
+    datos.remove(0,1);
+    digito = "";
+    count += 1;
+  }
 }
 
+//-----------------------------------------------------------------------------------------------------------------
 void subTracePoints(Point arrpoint[])
 {
+  //                                                      //We must move to the first point before droping the
+  //                                                      //    marker.
   subMoveToFirstPoint(arrpoint[0]);
 
-  for (int intI = 1; intI < 13; intI++)
+  //                                                      //Trace all the coordinates.
+  for (int intI = 1; intI < intPointsNumber; intI++)
   {
+
+    //                                                    //Must see which one is the next point's motor direction.
     if (arrpoint[intI].intX > arrpoint[intI - 1].intX)
     {
       stepperX.subSetDirection(stepperX.RIGHT);
@@ -88,20 +132,22 @@ void subTracePoints(Point arrpoint[])
   }
 }
 
+//-----------------------------------------------------------------------------------------------------------------
 void subMoveToFirstPoint(Point pointFirst)
 {
+ 
   stepperY.subSetDirection(stepperY.RIGHT);
   stepperX.subSetDirection(stepperX.RIGHT);
   stepperX.subMoveBySteps(pointFirst.intX * INT_X_SENSIVITY);
   stepperY.subMoveBySteps(pointFirst.intY * INT_Y_SENSIVITY);
   intCurrentX = pointFirst.intX;
   intCurrentY = pointFirst.intY;
+   servo.write(19);
 }
 
 //----------------------------------------------------------------------------------------------------
 void subTraceToPoint(Point pointNext)
 {
-  int intMax;
   int intNextX = pointNext.intX - intCurrentX;
   int intNextY = pointNext.intY - intCurrentY;
   if (intNextX < 0)
@@ -113,38 +159,57 @@ void subTraceToPoint(Point pointNext)
   {
     intNextY *= -1;
   }
-  
-  Serial.print(pointNext.intX);
-  Serial.print("-");
-  Serial.println(pointNext.intY);
-  Serial.print(intNextX);
-  Serial.print("-");
-  Serial.println(intNextY);
-  
-  if (intNextX > intNextY)
-  {
-    intMax = intNextX * INT_X_SENSIVITY;
-  }
-  else
-  {
-    intMax = intNextY * INT_Y_SENSIVITY;
-  }
 
   int intXLeft = intNextX * INT_X_SENSIVITY;
   int intYLeft = intNextY * INT_Y_SENSIVITY;
-
-  for (int intI = 0; intI < intMax; intI++)
+  int intDiagonalRatio = 1;
+  if (intXLeft > 0 && 
+      intYLeft > 0)
   {
-    if (intXLeft > 0)
+    if (intXLeft > intYLeft)
     {
-      stepperX.subMoveBySteps(1);
-      intXLeft -= 1;
+      intDiagonalRatio = intXLeft / intYLeft;
     }
-
-    if (intYLeft > 0)
+    else
     {
-      stepperY.subMoveBySteps(1);
-      intYLeft -= 1;
+      intDiagonalRatio = intYLeft / intXLeft;
+    }
+  }
+
+  if (intXLeft > intYLeft)
+  {
+    while (intXLeft > 0 ||
+         intYLeft > 0)
+    {
+      if (intXLeft > 0)
+      {
+        stepperX.subMoveBySteps(intDiagonalRatio);
+        intXLeft -= 1;
+      }
+  
+      if (intYLeft > 0)
+      {
+        stepperY.subMoveBySteps(1);
+        intYLeft -= 1;
+      }
+    }
+  }
+  else
+  {
+    while (intXLeft > 0 ||
+         intYLeft > 0)
+    {
+      if (intXLeft > 0)
+      {
+        stepperX.subMoveBySteps(1);
+        intXLeft -= 1;
+      }
+  
+      if (intYLeft > 0)
+      {
+        stepperY.subMoveBySteps(intDiagonalRatio);
+        intYLeft -= 1;
+      }
     }
   }
 
@@ -152,26 +217,11 @@ void subTraceToPoint(Point pointNext)
   intCurrentY = pointNext.intY;
 }
 
-//obtengo coordenadas del string de datos
-
-void getCoord(Point points[]){
-    while (datos.length() > 0){
-      while (isDigit(datos.charAt(0))){
-        digito += datos.charAt(0);
-        datos.remove(0,1);
-      }
-      points[count].intX = digito.toInt(); 
-      datos.remove(0,1);
-      digito = "";
-    
-      while (isDigit(datos.charAt(0))){
-        digito += datos.charAt(0);
-        datos.remove(0,1);
-      }
-      points[count].intY = digito.toInt(); 
-      datos.remove(0,1);
-      digito = "";
-    count += 1;
-  }
+//==================================================================================================
+//                                              //VOID LOOP
+void loop() 
+{
+  
 }
 
+//==================================================================================================
